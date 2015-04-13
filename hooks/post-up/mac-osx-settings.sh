b5198ef5b55ea8055affd6d1d3dadefc77ff175d
Christian Trabold <info@christian-trabold.de>
2015-04-14 02:46:53 +0800

    mac-osx-settings: Update defaults
    Based on https://github.com/mathiasbynens/dotfiles and
    http://nerdlogger.com/2012/07/30/get-control-of-mountain-lion-with-a-huge-list-of-command-line-tweaks/

--- hooks/post-up/mac-osx-settings.sh
+++ hooks/post-up/mac-osx-settings.sh
@@ -10,11 +10,31 @@ if [[ $OSTYPE == darwin* ]]; then
   # General UI/UX #
   ###############################################################################
 
+  # Disable the sound effects on boot
+  #sudo nvram SystemAudioVolume=" "
+
+  # Disable transparency in the menu bar and elsewhere on Yosemite
+  defaults write com.apple.universalaccess reduceTransparency -bool true
+
+  # Menu bar: show remaining battery time (on pre-10.8); hide percentage
+  defaults write com.apple.menuextra.battery ShowPercent -string "NO"
+  defaults write com.apple.menuextra.battery ShowTime -string "YES"
+
+  # Menu bar: hide the useless Time Machine and Volume icons
+  defaults write com.apple.systemuiserver menuExtras -array "/System/Library/CoreServices/Menu Extras/Bluetooth.menu" "/System/Library/CoreServices/Menu Extras/AirPort.menu" "/System/Library/CoreServices/Menu Extras/Battery.menu" "/System/Library/CoreServices/Menu Extras/Clock.menu"
+
+  # Disable auto-correct
+  defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+
   # Change Desktop default BackgroundColor to grey
   osascript -e 'tell application "Finder"' -e 'set theFile to POSIX file "/Library/Desktop Pictures/Solid Colors/Solid Aqua Graphite.png"' -e 'set desktop picture to (theFile as alias)' -e 'end tell'
 
   # Always show scrollbars
   defaults write NSGlobalDomain AppleShowScrollBars -string "Always"
+
+  # Increase window resize speed for Cocoa applications
+  defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
 
   # Expand save panel by default
   defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
@@ -27,8 +47,20 @@ if [[ $OSTYPE == darwin* ]]; then
   # Save to disk (not to iCloud) by default
   defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
 
+  # Automatically quit printer app once the print jobs complete
+  defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+  # Disable the “Are you sure you want to open this application?” dialog
+  #defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+  # Disable the crash reporter
+  #defaults write com.apple.CrashReporter DialogType -string "none"
+
   # Set Help Viewer windows to non-floating mode
   defaults write com.apple.helpviewer DevMode -bool true
+
+  # Disable Notification Center and remove the menu bar icon
+  #launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist 2> /dev/null
 
   # Disable smart quotes as they’re annoying when typing code
   defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
@@ -62,8 +94,15 @@ if [[ $OSTYPE == darwin* ]]; then
   # Screen #
   ###############################################################################
 
-  # Save screenshots to the desktop
+  # Require password immediately after sleep or screen saver begins
+  defaults write com.apple.screensaver askForPassword -int 1
+  defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+  # Save screenshots to a dedicated folder
   defaults write com.apple.screencapture location ~/Pictures/Screenshots
+
+  # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+  defaults write com.apple.screencapture type -string "png"
 
   # Disable shadow in screenshots
   defaults write com.apple.screencapture disable-shadow -bool true
@@ -73,8 +112,17 @@ if [[ $OSTYPE == darwin* ]]; then
   # Finder #
   ###############################################################################
 
+  # Finder: allow quitting via ⌘ + Q; doing so will also hide desktop icons
+  defaults write com.apple.finder QuitMenuItem -bool true
+
   # Finder: disable window animations and Get Info animations
   defaults write com.apple.finder DisableAllAnimations -bool true
+
+  # Show icons for hard drives, servers, and removable media on the desktop
+  defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+  defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
+  defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
+  defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 
   # Finder: show hidden files by default
   defaults write com.apple.finder AppleShowAllFiles -bool true
@@ -114,6 +162,28 @@ if [[ $OSTYPE == darwin* ]]; then
   defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
   defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
 
+  # Show item info below icons on the desktop and in other icon views
+  /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:showItemInfo true" ~/Library/Preferences/com.apple.finder.plist
+  /usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:showItemInfo true" ~/Library/Preferences/com.apple.finder.plist
+  /usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:showItemInfo true" ~/Library/Preferences/com.apple.finder.plist
+
+  # Enable snap-to-grid for icons on the desktop and in other icon views
+  /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+  /usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+  /usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+
+  # Increase grid spacing for icons on the desktop and in other icon views
+  /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:gridSpacing 100" ~/Library/Preferences/com.apple.finder.plist
+  /usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:gridSpacing 100" ~/Library/Preferences/com.apple.finder.plist
+  /usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:gridSpacing 100" ~/Library/Preferences/com.apple.finder.plist
+
+  # Increase the size of icons on the desktop and in other icon views
+  /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:iconSize 80" ~/Library/Preferences/com.apple.finder.plist
+  /usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:iconSize 80" ~/Library/Preferences/com.apple.finder.plist
+  /usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:iconSize 80" ~/Library/Preferences/com.apple.finder.plist
+
+
+
   # Use list view in all Finder windows by default
   # Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
   defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
@@ -124,9 +194,9 @@ if [[ $OSTYPE == darwin* ]]; then
   # Expand the following File Info panes:
   # “General”, “Open with”, and “Sharing & Permissions”
   defaults write com.apple.finder FXInfoPanesExpanded -dict \
-  General -bool true \
-  OpenWith -bool true \
-  Privileges -bool true
+    General -bool true \
+    OpenWith -bool true \
+    Privileges -bool true
 
 
   ###############################################################################
@@ -158,6 +228,9 @@ if [[ $OSTYPE == darwin* ]]; then
 
   # Don’t show Dashboard as a Space
   defaults write com.apple.dock dashboard-in-overlay -bool true
+
+  # Reset Launchpad
+  find ~/Library/Application\ Support/Dock -name "*.db" -maxdepth 1 -delete
 
   # Hot corners
   # Possible values:
@@ -192,8 +265,14 @@ if [[ $OSTYPE == darwin* ]]; then
   # Hide Safari’s bookmarks bar by default
   defaults write com.apple.Safari ShowFavoritesBar -bool false
 
+  # Disable Safari’s thumbnail cache for History and Top Sites
+  defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2
+
   # Remove useless icons from Safari’s bookmarks bar
   defaults write com.apple.Safari ProxiesInBookmarksBar "()"
+
+  # Enable Safari’s debug menu
+  defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
 
   # Enable the Develop menu and the Web Inspector in Safari
   defaults write com.apple.Safari IncludeDevelopMenu -bool true
@@ -268,6 +347,31 @@ if [[ $OSTYPE == darwin* ]]; then
 
 
   ###############################################################################
+  # Google Chrome & Google Chrome Canary                                        #
+  ###############################################################################
+
+  # Allow installing user scripts via GitHub Gist or Userscripts.org
+  defaults write com.google.Chrome ExtensionInstallSources -array "https://gist.githubusercontent.com/" "http://userscripts.org/*"
+  defaults write com.google.Chrome.canary ExtensionInstallSources -array "https://gist.githubusercontent.com/" "http://userscripts.org/*"
+
+  # Disable the all too sensitive backswipe on trackpads
+  defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool false
+  defaults write com.google.Chrome.canary AppleEnableSwipeNavigateWithScrolls -bool false
+
+  # Disable the all too sensitive backswipe on Magic Mouse
+  defaults write com.google.Chrome AppleEnableMouseSwipeNavigateWithScrolls -bool false
+  defaults write com.google.Chrome.canary AppleEnableMouseSwipeNavigateWithScrolls -bool false
+
+  # Use the system-native print preview dialog
+  defaults write com.google.Chrome DisablePrintPreview -bool true
+  defaults write com.google.Chrome.canary DisablePrintPreview -bool true
+
+  # Expand the print dialog by default
+  defaults write com.google.Chrome PMPrintingExpandedStateForPrint2 -bool true
+  defaults write com.google.Chrome.canary PMPrintingExpandedStateForPrint2 -bool true
+
+
+  ###############################################################################
   # GPGMail 2 #
   ###############################################################################
 
@@ -288,9 +392,42 @@ if [[ $OSTYPE == darwin* ]]; then
   # iTunes #
   ###############################################################################
 
-  #  Changes arrows next to artists and albums to search your library instead of iTunes Store (0 to revert)
-  defaults write com.apple.iTunes invertStoreLinks 1
+  # Make the iTunes arrow links go to your library instead of the iTunes Store
+  defaults write com.apple.iTunes invertStoreLinks -bool true
 
   # Turn off backups of iPhone/iPod Touch in iTunes during sync (0 to re-enable)
   defaults write com.apple.iTunes DeviceBackupsDisabled 1
+
+  # Disable the Ping sidebar in iTunes
+  defaults write com.apple.iTunes disablePingSidebar -bool true
+
+  # Disable all the other Ping stuff in iTunes
+  defaults write com.apple.iTunes disablePing -bool true
+
+  # Make ⌘ + F focus the search input in iTunes
+  defaults write com.apple.iTunes NSUserKeyEquivalents -dict-add "Target Search Field" "@F"
+
+
+  ###############################################################################
+  # Terminal & iTerm 2                                                          #
+  ###############################################################################
+
+  # Only use UTF-8 in Terminal.app
+  defaults write com.apple.terminal StringEncodings -array 4
+
+  # Enable “focus follows mouse” for Terminal.app and all X11 apps
+  # This means you can hover over a window and start typing in it without clicking first
+  defaults write com.apple.terminal FocusFollowsMouse -bool true
+  defaults write org.x.X11 wm_ffm -bool true
+
+  # Don’t display the annoying prompt when quitting iTerm
+  defaults write com.googlecode.iterm2 PromptOnQuit -bool false
+
+
+  ###############################################################################
+  # Time Machine #
+  ###############################################################################
+
+  # Prevent Time Machine from prompting to use new hard drives as backup volume
+  defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 fi
